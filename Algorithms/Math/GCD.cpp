5881be8367cbfb940ff76f633338f4ef6a2d08da
matheusmtta <matheusaquino199@gmail.com>
2020-04-02 07:17:26 -0300

    GCD and LCM added

---
+++ Algorithms/Math/GCD.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define INF (int)0x3f3f3f3f
+#define LINF (long long)0x3f3f3f3f3f3f3f3fll
+#define MP make_pair
+#define PB push_back
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+int gcd (int a, int b) {
+    return b ? gcd (b, a % b) : a;
+}
+
+int main(){	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	return 0;
+}
---
+++ Algorithms/Math/LCM.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define INF (int)0x3f3f3f3f
+#define LINF (long long)0x3f3f3f3f3f3f3f3fll
+#define MP make_pair
+#define PB push_back
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+ 
+int lcm (int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+int main(){	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	return 0;
+}
