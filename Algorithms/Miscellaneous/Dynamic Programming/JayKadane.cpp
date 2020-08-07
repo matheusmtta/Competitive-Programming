3abc8c1b7fcfaa54238857495a4fa2492912429e
matheusmtta <matheusaquino199@gmail.com>
2020-08-07 18:40:56 -0300

    Adding JayKadane Algorithm, Maximum Range Sum 1D/2D Queries

--- Algorithms/Miscellaneous/Dynamic Programming/JayKadane.cpp
+++ Algorithms/Miscellaneous/Dynamic Programming/JayKadane.cpp
@@ -44,12 +44,9 @@ int MaxRangeSum2D(){
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++){
 			cin >> matrix[i][j];
-			if (i > 0) 
-				matrix[i][j] += matrix[i-1][j];
-			if (j > 0)
-			    matrix[i][j] += matrix[i][j-1];
-			if (i > 0 && j > 0)
-				 matrix[i][j] -= matrix[i-1][j-1];
+			if (i > 0) matrix[i][j] += matrix[i-1][j];
+			if (j > 0) matrix[i][j] += matrix[i][j-1];
+			if (i > 0 && j > 0) matrix[i][j] -= matrix[i-1][j-1];
 		}
 	}
 
