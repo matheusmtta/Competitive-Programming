ade226aecda80d1b0aef102472074a22adbaa97e
matheusmtta <matheusaquino199@gmail.com>
2020-10-07 23:27:56 -0300

    Corrigindo um erro gritante, jaykadane 2d

--- Algorithms/Dynamic Programming/JayKadane/JayKadane2D.cpp
+++ Algorithms/Dynamic Programming/JayKadane/JayKadane2D.cpp
@@ -13,34 +13,49 @@ typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int  uint64;
 
-//Maximum 2D Range Sum O(n^4)
+//Maximum 2D Range Sum O(n^3)
+
+int MaxRangeSum1D(vector <int> arr){
+	int n = arr.size();
+
+ 	int ans = -INF, sum = 0, tmp = 0, l = 0, r = 0;
+
+ 	for (int i = 0; i < n; i++){
+ 		sum += arr[i];
+ 		if (sum < 0){ 
+ 			sum = 0;
+ 			tmp = i + 1;
+ 		}
+ 		else if (sum > ans) {
+ 			ans = max(sum, ans);
+ 			l = tmp;
+ 			r = i;
+ 		}
+ 	}
+
+ 	return ans;
+}
+
 
 int MaxRangeSum2D(){
-	int n; cin >> n;
-	vector <vector<int>> matrix(n, vector<int> (n));
+	int n, m; cin >> n >> m;
+	vector <vector<int>> matrix(n+1, vector<int> (m+1, 0));
 
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n; j++){
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= m; j++){
 			cin >> matrix[i][j];
-			if (i > 0) matrix[i][j] += matrix[i-1][j];
-			if (j > 0) matrix[i][j] += matrix[i][j-1];
-			if (i > 0 && j > 0) matrix[i][j] -= matrix[i-1][j-1];
+			matrix[i][j] += matrix[i][j-1];
 		}
 	}
 
 	int ans = -INF;
 
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n; j++){
-			for (int r = i; r < n; r++){
-				for (int c = j; c < n; c++){
-					int submtx = matrix[r][c]; //sum from (0, 0) to (r, c)
-					if (i > 0) submtx -= matrix[i-1][c];
-					if (j > 0) submtx -= matrix[r][j-1];
-					if (i > 0 && j > 0) submtx += matrix[i-1][j-1];
-					ans = max(ans, submtx);
-				}
-			}
+	for (int i = 1; i <= m; i++){
+		vector <int> aux(n+1, 0);
+		for (int j = i; j <= m; j++){
+			for (int k = 1; k <= n; k++)
+				aux[k] = matrix[k][j] - matrix[k][i-1];
+			ans = max(MaxRangeSum1D(aux), ans);
 		}
 	}
 
@@ -51,7 +66,6 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	MaxRangeSum1D();
 	MaxRangeSum2D();
 
 	return 0;
