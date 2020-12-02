0ee103f07dfadfa4e00b8d0c14cc53e25b4376dc
matheusmtta <matheusaquino199@gmail.com>
2020-12-02 18:38:34 -0300

    Changing template and fixing some stuff

--- Algorithms/Math/pascalTriangle.cpp
+++ Algorithms/Math/pascalTriangle.cpp
@@ -18,9 +18,9 @@ int64 C[MAX][MAX];
 void build(){
 	C[0][0] = 1;
 
-	for (int i = 1; i <= n; j++)
+	for (int i = 1; i <= n; i++)
 		C[i][0] = C[i][i] = 1;
-		for (int j = 1; j < n; j++)
+		for (int j = 1; j <= n; j++)
 			C[i][j] = (C[i - 1][j - 1] + C[i - 1][j])%MOD;
 }
 
--- template.cpp
+++ template.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 #define MOD 1000000007
 #define end "\n"
-#define first f
-#define second s
+#define f first
+#define s second
 #define INF (int)0x3f3f3f3f
 #define LINF (long long)0x3f3f3f3f3f3f3f3fll
 
